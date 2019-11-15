03b2301c286d550d051fd350fbeac47b36bee84e
Evan Cleary <evanjcleary@gmail.com>
2019-11-15 18:01:14 -0500

    removed unnecessary code

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json( {type: "application/vnd.api+json"}));
 
 app.use(express.static("app/public"));
 
